2d6d1a28a18c9d3d573f759e3e52c8de728bfc51
Gustavo Perin <gustavoperin067@gmail.com>
2025-01-26 03:46:04 +0000

    build: update `devServer` settings to support `Codespaces`

--- vue.config.cjs
+++ vue.config.cjs
@@ -10,4 +10,17 @@ module.exports = defineConfig({
       title: "Card web",
     },
   },
+  devServer: {
+    host: "0.0.0.0", // Exponha para ser acessível no Codespaces
+    port: 8080, // Porta padrão
+    //https: true, // Força HTTPS
+    //allowedHosts: "all", // Permite qualquer host se conectar
+    client: {
+      webSocketURL: {
+        hostname: "0.0.0.0",
+        port: 443, // Porta HTTPS padrão
+        protocol: "wss", // Use WebSocket seguro
+      },
+    },
+  },
 });
